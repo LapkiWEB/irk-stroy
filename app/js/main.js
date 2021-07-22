74122eefd8dde72f159b4abf53d04289ca41d808
Stepan <stefan.irk96@mail.ru>
2021-07-22 17:53:57 +0800

    finish, no seo

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,55 @@ $(document).ready(function () {
         }
     });
 });
+
+$('.slider__inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [{
+            breakpoint: 1700,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 1450,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 704,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+let secret = document.querySelector('#secret');
+document.querySelector('.contacts__form-btn').onclick = function () {
+    secret.value = 'secretkey';
+}
